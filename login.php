da7f58f05908decc90e23c670da84d406052b07b
Zhanel Druskuldinova <104449365+210103031@users.noreply.github.com>
2022-12-20 00:59:47 +0600

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 
 session_start();
  
-if (isset ($_SESSION["loggedin"]) && $_SESSION["loggein"] === true{
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true{
  header ("location welcme.php");
  exit;
 }
