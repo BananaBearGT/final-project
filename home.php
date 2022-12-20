bc5320b1e49f2b6e958de14127a5a56cb0bc6dc6
BananaBearGT <90473358+BananaBearGT@users.noreply.github.com>
2022-12-20 11:22:29 +0600

    Add files via upload

--- home.php
+++ home.php
@@ -10,8 +10,118 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <head>
   <title>My Clothes Store</title>
   <link rel="stylesheet" type="text/css" href="style.css">
+  <style type="text/css">
+    * {box-sizing:border-box}
+
+    .slideshow-container {
+      max-width: 1000px;
+      position: relative;
+      margin: auto;
+    }
+
+    .mySlides {
+      display: none;
+    }
+
+    .prev, .next {
+      cursor: pointer;
+      position: absolute;
+      top: 50%;
+      width: auto;
+      margin-top: -22px;
+      padding: 16px;
+      color: white;
+      font-weight: bold;
+      font-size: 18px;
+      transition: 0.6s ease;
+      border-radius: 0 3px 3px 0;
+      user-select: none;
+    }
+
+    .next {
+      right: 0;
+      border-radius: 3px 0 0 3px;
+    }
+
+    .prev:hover, .next:hover {
+      background-color: rgba(0,0,0,0.8);
+    }
+
+    .text {
+      font-weight: bold;
+      color: white;
+      opacity: 0.90;
+      font-size: 40px;
+      padding: 8px 12px;
+      position: absolute;
+      bottom: 300px;
+      width: 100%;
+      text-align: center;
+    }
+
+    .numbertext {
+      color: #f2f2f2;
+      font-size: 12px;
+      padding: 8px 12px;
+      position: absolute;
+      top: 0;
+    }
+
+    .dot {
+      cursor: pointer;
+      height: 15px;
+      width: 15px;
+      margin: 0 2px;
+      background-color: #bbb;
+      border-radius: 50%;
+      display: inline-block;
+      transition: background-color 0.6s ease;
+    }
+
+    .active, .dot:hover {
+      background-color: #717171;
+    }
+
+    .fade {
+      animation-name: fade;
+      animation-duration: 1.5s;
+    }
+
+    @keyframes fade {
+      from {opacity: .4}
+      to {opacity: 1}
+    }
+  </style>
+  <script type="text/javascript"> 
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";
+      dots[slideIndex-1].className += " active";
+    }
+  </script>
 </head>
-<body>
+<body onload="currentSlide(1)">
   <header>
     <nav>
       <ul>
@@ -25,7 +135,36 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     </nav>
   </header>
   <main>
-    <h1 style="background-image: url(https://media.timeout.com/images/101714607/image.jpg); height: 500px;background-repeat: no-repeat;">Welcome to My Clothes Store</h1>
+      <div class="slideshow-container">
+
+        <div class="mySlides fade">
+          <div class="numbertext">1 / 3</div>
+          <img src="https://media.timeout.com/images/101714607/image.jpg" style="width:100%">
+          <div class="text" style="">Welcome to My Clothes Store!</div>
+        </div>
+
+        <div class="mySlides fade">
+          <div class="numbertext">2 / 3</div>
+          <img src="https://media.istockphoto.com/id/1222457390/photo/small-business-owener-stock-photo.jpg?s=612x612&w=0&k=20&c=aoiYnGS1HIPf7ZGyn68-NuYu-d_JmPfz2UYC2vllDPs=" style="width:100%">
+          <div class="text">We glad to see you!</div>
+        </div>
+
+        <div class="mySlides fade">
+          <div class="numbertext">3 / 3</div>
+          <img src="https://i.pinimg.com/originals/99/4e/1b/994e1b7fae8f8f69decc1745679d144b.jpg" style="width:100%">
+          <div class="text">Hope you`ll enjoy the shoping</div>
+        </div>
+
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+      </div>
+      <br>
+
+      <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+      </div>
     <p style="text-align: center; font-weight: 100; font-size: large; margin-bottom: 20px;">We offer a wide range of high-quality clothes for men, women, and children.</p>
     <div class="featured-products">
       <div class="product">
