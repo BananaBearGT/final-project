df133805e65c12691c54a178f249e447f7d84214
Zhanel Druskuldinova <104449365+210103031@users.noreply.github.com>
2022-12-20 07:50:11 +0600

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define ('DB_PASSWORD', '');
-define ('DB-NAME');
+define ('DB-NAME', 'web');
  
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
